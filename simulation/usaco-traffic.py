1d20c72c0b239277f7d44c27da5478b7363126a5
Alexandre Riedo <alexandre.riedo@etu.unige.ch>
2026-04-15 01:47:49 +0200

    wip: usaco-bronze Measuring Traffic

--- simulation/usaco-traffic.py
+++ simulation/usaco-traffic.py
@@ -13,33 +13,46 @@ for _ in range(num_miles):
     upper = int(upper)
     segments.append((ramp_presence, lower, upper))
 
-prior = [0, MAX_SENSOR_RANGE]
-for ramp_presence, lower, upper in reversed(segments):
+prior = None
+i = len(segments)
+while prior is None:
+    i -= 1
+    if segments[i][0] == "none":
+        prior = [segments[i][1], segments[i][2]]
+
+for ramp_presence, lower, upper in reversed(segments[:i]):
     if ramp_presence == "none":
         prior[0] = max(prior[0], lower)
         prior[1] = min(prior[1], upper)
 
     if ramp_presence == "off":
-        prior[0] = prior[0] + upper
-        prior[1] = prior[1] + lower
+        prior[0] = prior[0] + lower
+        prior[1] = prior[1] + upper
 
     if ramp_presence == "on":
         prior[0] = prior[0] - upper
         prior[1] = prior[1] - lower
 
-after = [0, MAX_SENSOR_RANGE]
-for ramp_presence, lower, upper in segments:
+
+after = None
+j = -1
+while after is None:
+    j += 1
+    if segments[j][0] == "none":
+        after = [segments[j][1], segments[j][2]]
+
+for ramp_presence, lower, upper in segments[j + 1 :]:
     if ramp_presence == "none":
         after[0] = max(after[0], lower)
         after[1] = min(after[1], upper)
 
-    if ramp_presence == "on":
-        after[0] = after[0] + upper
-        after[1] = after[1] + lower
-
     if ramp_presence == "off":
         after[0] = after[0] - upper
         after[1] = after[1] - lower
+
+    if ramp_presence == "on":
+        after[0] = after[0] + lower
+        after[1] = after[1] + upper
 
 print(" ".join(map(str, prior)))
 print(" ".join(map(str, after)))
@@ -51,6 +64,7 @@ on 1 4
 ?
 
 10 10 is the most specific?
+7 13 seems the best
 
 """
 
@@ -66,8 +80,8 @@ on 2 3
 
 """
 ?
-off 2 3
+off 4 6
 6 10
 
-9 12 is the most specific
+12 14 is the most specific
 """
