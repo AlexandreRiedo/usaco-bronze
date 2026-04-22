f4b3ae9aa69ad544227651d7fc0183ef5c76d3ae
Alexandre Riedo <alexandre.riedo@etu.unige.ch>
2026-04-22 22:04:33 +0200

    wip: usaco-bronze Cow Checkups (marginal heuristics improvements)

--- basic-complete-search/usaco-cowcheckups.py
+++ basic-complete-search/usaco-cowcheckups.py
@@ -11,19 +11,45 @@ def count_checked(proposed_order, vet_order):
     return count
 
 
+def build_left_checked(farmer_order, vet_order, num_cows):
+    checked_left = [0 for _ in range(num_cows)]
+    for index, (farmer_cow, vet_cow) in enumerate(zip(farmer_order, vet_order)):
+        if index != 0:
+            checked_left[index] = checked_left[index - 1]
+        if farmer_cow == vet_cow:
+            checked_left[index] += 1
+    return checked_left
+
+
+def build_right_checked(farmer_order, vet_order, num_cows):
+    checked_right = [0 for _ in range(num_cows)]
+    for index, (farmer_cow, vet_cow) in enumerate(
+        zip(reversed(farmer_order), reversed(vet_order))
+    ):
+        if index != 0:
+            checked_right[index] = checked_right[index - 1]
+        if farmer_cow == vet_cow:
+            checked_right[index] += 1
+    checked_right.reverse()
+    return checked_right
+
+
+checked_left = [0] + build_left_checked(farmer_order, vet_order, num_cows)
+checked_right = build_right_checked(farmer_order, vet_order, num_cows) + [0]
+
 checked_count = {i: 0 for i in range(num_cows + 1)}
 for left in range(num_cows):
     for right in range(left + 1, num_cows + 1):
         checked_count[
             count_checked(
-                farmer_order[:left]
-                + list(reversed(farmer_order[left:right]))
-                + farmer_order[right:],
-                vet_order,
+                list(reversed(farmer_order[left:right])),
+                vet_order[left:right],
             )
+            + checked_left[left]
+            + checked_right[right]
         ] += 1
 
-for checked, count in sorted(checked_count.items(), key=lambda kv: kv[0]):
+for _, count in sorted(checked_count.items(), key=lambda kv: kv[0]):
     print(count)
 
 """
