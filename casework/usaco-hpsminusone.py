71c484d1df6f3db1e8c5bf4ede84874c8036d776
Alexandre Riedo <alexandre.riedo@etu.unige.ch>
2026-05-04 03:02:46 +0200

    wip: usaco-bronze Hoof Paper Scissors Minus One (6/12, need to work on deduplication)

--- casework/usaco-hpsminusone.py
+++ casework/usaco-hpsminusone.py
@@ -18,10 +18,6 @@ for i in range(1, num_symbols + 1):
             loses[i].add(j)
             wins[j].add(i)
 
-# rprint(f"{draws=}")
-# rprint(f"{wins=}")
-# rprint(f"{loses=}")
-
 for _ in range(num_games):
     elsie_left, elsie_right = map(int, input().split())
     count = 0
@@ -31,11 +27,11 @@ for _ in range(num_games):
     losing_symbols = wins[elsie_left] | wins[elsie_right]
     left_killer_symbols = loses[elsie_left] - wins[elsie_right]
     right_killer_symbols = loses[elsie_right] - wins[elsie_left]
-    # rprint(f"{drawing_symbols=}")
-    # rprint(f"{losing_symbols=}")
-    # rprint(f"{killer_symbols=}")
-    # rprint(f"{left_killer_symbols=}")
-    # rprint(f"{right_killer_symbols=}")
+
+    # Heuristic
+    if len(left_killer_symbols) + len(right_killer_symbols) + len(killer_symbols) == 0:
+        print(0)
+        continue
 
     # Case (left_killer, right_killer + draw breaker)
     left_case = {
@@ -45,22 +41,55 @@ for _ in range(num_games):
     }
 
     # Case (left_killer + draw breaker, right_killer)
+    to_exclude = set(left_case)
     right_case = {
         (left, right)
         for left in (left_killer_symbols - draws[elsie_right])
         for right in right_killer_symbols
+        if (left, right) not in to_exclude
     }
 
     # Case (killer, loses)
-    case_1 = {(killer, lose) for killer in killer_symbols for lose in losing_symbols}
+    to_exclude.update(right_case)
+    case_1 = {
+        (killer, lose)
+        for killer in killer_symbols
+        for lose in losing_symbols
+        if (killer, lose) not in to_exclude
+    }
 
     # Case (killer, draws)
-    case_2 = {(killer, draw) for killer in killer_symbols for draw in drawing_symbols}
+    to_exclude.update(case_1)
+    case_2 = {
+        (killer, draw)
+        for killer in killer_symbols
+        for draw in drawing_symbols
+        if (killer, draw) not in to_exclude
+    }
 
     # Case (loses, killer)
-    case_3 = {(lose, killer) for killer in killer_symbols for lose in losing_symbols}
+    to_exclude.update(case_2)
+    case_3 = {
+        (lose, killer)
+        for killer in killer_symbols
+        for lose in losing_symbols
+        if (lose, killer) not in to_exclude
+    }
 
     # Case (draws, killer)
-    case_4 = {(draw, killer) for killer in killer_symbols for draw in drawing_symbols}
+    to_exclude.update(case_3)
+    case_4 = {
+        (draw, killer)
+        for killer in killer_symbols
+        for draw in drawing_symbols
+        if (draw, killer) not in to_exclude
+    }
 
-    print(len(left_case | right_case | case_1 | case_2 | case_3 | case_4))
+    print(
+        len(left_case)
+        + len(right_case)
+        + len(case_1)
+        + len(case_2)
+        + len(case_3)
+        + len(case_4)
+    )
