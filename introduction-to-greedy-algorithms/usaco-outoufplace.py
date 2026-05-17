a4cf16eadc68e6e102672158ca4e91d73d3e913c
Alexandre Riedo <alexandre.riedo@etu.unige.ch>
2026-05-18 00:09:38 +0200

    solve: usaco-bronze Out Of Place

--- introduction-to-greedy-algorithms/usaco-outoufplace.py
+++ introduction-to-greedy-algorithms/usaco-outoufplace.py
@@ -12,28 +12,11 @@ for idx, cow in enumerate(sorted_cows):
 res = 0
 for (idx_a, cow_a), (idx_b, cow_b) in zip(enumerate(cows), enumerate(cows[1:], 1)):
     if cow_b < cow_a:
-        a_left, a_right = (
-            min(idx_a, min(cow_to_idx[cow_a])),
-            max(idx_a, max(cow_to_idx[cow_a])),
-        )
-        b_left, b_right = (
-            min(idx_b, min(cow_to_idx[cow_b])),
-            max(idx_b, max(cow_to_idx[cow_b])),
-        )
-        a_dist = len(set(sorted_cows[a_left:a_right]) - {cow_a})
-        b_dist = len(set(sorted_cows[b_left:b_right]) - {cow_b})
-        res = max(a_dist + 1, b_dist + 1, 0)
-
-        if a_dist == 0 and b_dist == 0:
-            res = 0
+        a_left, a_right = (idx_a, min(cow_to_idx[cow_a]))
+        b_left, b_right = (max(cow_to_idx[cow_b]), idx_b)
+        a_dist = len(set(sorted_cows[a_left : a_right + 1]) - {cow_a})
+        b_dist = len(set(sorted_cows[b_left : b_right + 1]) - {cow_b})
+        res = max(a_dist, b_dist)
 
 with open("outofplace.out", "w") as f:
     f.write(f"{res}\n")
-
-"""
-2 4 7 7 9 3
-
-2 4 3 7 7 9 
-
-2 3 4 9 7 7
-"""
