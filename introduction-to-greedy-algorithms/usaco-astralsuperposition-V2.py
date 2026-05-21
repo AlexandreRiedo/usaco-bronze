fa6bee1aa6bdf56a39b1ad05cf45aa137fedb24b
Alexandre Riedo <alexandre.riedo@etu.unige.ch>
2026-05-21 16:33:43 +0200

    wip: faster but flawed algo for usaco-bronze Astral Superposition

--- introduction-to-greedy-algorithms/usaco-astralsuperposition-V2.py
+++ introduction-to-greedy-algorithms/usaco-astralsuperposition-V2.py
@@ -7,8 +7,35 @@ class Point(NamedTuple):
     y: int
 
 
-def valid_back_shift(point, right, down):
-    return (point.x - right) >= 0 and (point.y - down) >= 0
+def solve(photo, photo_size, shift_right, shift_down):
+    count = 0
+    for y in range(photo_size):
+        for x in range(photo_size):
+            char = photo[y][x]
+            back_char = (
+                photo[y - shift_down][x - shift_right]
+                if (y - shift_down >= 0 and x - shift_right >= 0)
+                else None
+            )
+            foward_char = (
+                photo[y + shift_down][x + shift_right]
+                if (y + shift_down < photo_size and x + shift_right < photo_size)
+                else None
+            )
+
+            if char == "W":
+                continue
+            elif char == "G":
+                if foward_char == "G" or foward_char == "B":
+                    count += 1
+                elif back_char == "W" or back_char is None:
+                    count += 1
+            elif char == "B":
+                if back_char is None or back_char == "W":
+                    return -1
+                else:
+                    count += 1
+    return count
 
 
 num_tests = int(sys.stdin.readline().strip())
@@ -16,38 +43,13 @@ num_tests = int(sys.stdin.readline().strip())
 for _ in range(num_tests):
     # 0. Setup
     photo_size, shift_right, shift_down = map(int, sys.stdin.readline().strip().split())
-    count = 0
-
     photo = [list(sys.stdin.readline().strip()) for _ in range(photo_size)]
 
     # 1. Algorithm
-    for y in range(photo_size - 1, -1, -1):
-        for x in range(photo_size - 1, -1, -1):
-            char = photo[y][x]
-            if char == "W":
-                continue
-            if char == "G":
-                if x >= shift_right and y >= shift_down:
-                    if photo[y - shift_down][x - shift_right] == "W":
-                        count += 1
-                else:
-                    count += 1
-            if char == "B":
-                if x >= shift_right and y >= shift_down:
-                    if photo[y - shift_down][x - shift_right] == "W":
-                        count = -1
-                        break
-                    else:
-                        count += 1
-                count += 1
+    answer = solve(photo, photo_size, shift_right, shift_down)
 
-    # x. Output!
-    sys.stdout.write(f"{count}\n")
-
-    # Testing
-    from rich import print as rprint
-
-    rprint(photo)
+    # 2. Output!
+    sys.stdout.write(f"{answer}\n")
 
 """
 CASE 1
