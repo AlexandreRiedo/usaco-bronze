df419a264cc84e9730009c891d1f65cf46598db2
Alexandre Riedo <alexandre.riedo@etu.unige.ch>
2026-05-21 17:29:50 +0200

    chore: cleanup of usaco-bronze Astral Superposition

--- introduction-to-greedy-algorithms/usaco-astralsuperposition-V2.py
+++ introduction-to-greedy-algorithms/usaco-astralsuperposition-V2.py
@@ -1,10 +1,4 @@
 import sys
-from typing import NamedTuple
-
-
-class Point(NamedTuple):
-    x: int
-    y: int
 
 
 def solve(photo, photo_size, shift_right, shift_down):
@@ -13,13 +7,13 @@ def solve(photo, photo_size, shift_right, shift_down):
     for y in range(photo_size):
         for x in range(photo_size):
             char = photo[y][x]
-            pos = (x, y)
-            back_pos = (x - shift_right, y - shift_down)
             back_char = (
                 photo[y - shift_down][x - shift_right]
                 if ((y - shift_down) >= 0 and (x - shift_right) >= 0)
                 else None
             )
+            pos = (x, y)
+            back_pos = (x - shift_right, y - shift_down)
 
             if char == "W":
                 continue
@@ -30,7 +24,7 @@ def solve(photo, photo_size, shift_right, shift_down):
                 if back_char is None or back_char == "W":
                     return -1
                 else:
-                    if back_pos not in used_pos and back_pos is not None:
+                    if back_pos not in used_pos:
                         used_pos.add(pos)
                         used_pos.add(back_pos)
                     else:
@@ -51,62 +45,3 @@ for _ in range(num_tests):
 
     # 2. Output!
     sys.stdout.write(f"{answer}\n")
-
-"""
-CASE 1
-1
-5 1 2
-GWGWW
-WGWWW
-WBWGW
-WWWWW
-WWGWW
--> 4
-
-*.*..
-.*...
-.*...
-.....
-.....
-
-.....
-.....
-.*.*.
-..#..
-..*..
-"""
-
-"""
-CASE 2
-1
-3 1 1
-WWW
-WBW
-WWW
--> -1
-
-*..
-.*.
-...
-can't work !
-
-WWWWBWWWW
-"""
-
-
-"""
-CASE 3
-3 1 0
-GGB
-GGW
-WWW
--> 4
-
-***
-*..
-...
-
-..*
-.*.
-...
-"""
