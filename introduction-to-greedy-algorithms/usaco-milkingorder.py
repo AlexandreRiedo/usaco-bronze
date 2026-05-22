39a6fd0e577f6913dedcc811b79030ddfe3cbce4
Alexandre Riedo <alexandre.riedo@etu.unige.ch>
2026-05-22 19:19:02 +0200

    wip: usaco-bronze Milking Order (6/10 solved)

--- introduction-to-greedy-algorithms/usaco-milkingorder.py
+++ introduction-to-greedy-algorithms/usaco-milkingorder.py
@@ -1,63 +1,45 @@
-from collections import defaultdict
-
-from rich import print as rprint
-
-
-def find_next_idx(milking_order, start):
-    for i in range(start, len(milking_order)):
-        if milking_order[i] == -1:
-            continue
-        else:
-            break
-    else:
-        return None
-
-    return milking_order[i]
-
-
 with open("milkorder.in") as file:
-    num_cows, _, num_position = map(int, file.readline().split())
-    cow_to_hierarchy = {
-        cow: idx for idx, cow in enumerate(map(int, file.readline().split()), 1)
-    }
-    hierarchy_list = list(cow_to_hierarchy.keys())
-    ordering = defaultdict(int)
+    num_cows, num_hierarchy, num_position = map(int, file.readline().strip().split())
+    hierarchy = [
+        int(num) for num in reversed(file.readline().strip().split())
+    ]  # highest priorty at [-1]
+    ordering = []
     for _ in range(num_position):
-        cow, order = map(int, file.readline().split())
-        ordering[order] = cow
-
-hierarchy_left_idx = 0
-
-milking_order = [-1 for _ in range(num_cows)]
-used_cows = set()
-for order, cow in ordering.items():
-    milking_order[order - 1] = cow
-    used_cows.add(cow)
-
-curr_hierarchy_idx = 0
-curr_milking_idx = 0
-while True:
-    if milking_order[curr_milking_idx] != -1:
-        curr_milking_idx += 1
-        used_cows.add(milking_order[curr_milking_idx])
-    else:
-        next_idx = find_next_idx(milking_order, curr_milking_idx)
-        if next_idx is None:
-            break
-        next_value = milking_order[next_idx]
-
-        if next_value not in hierarchy_list:
-            break
+        cow, order = map(int, file.readline().strip().split())
+        ordering.append((cow, order - 1))
+    ordering.sort(key=lambda x: x[1], reverse=True)  # highest priority at [-1]
 
 
-rprint(f"{num_cows=} {num_position=}")
-rprint(f"{cow_to_hierarchy=}")
-rprint(f"{hierarchy_list=}")
-rprint(f"{ordering=}")
-rprint(f"{milking_order=}")
-rprint(f"{used_cows=}")
-rprint(f"{curr_milking_idx=}")
+def solve(hierarchy, ordering, num_cows):
+    for idx in range(num_cows):
+        if not ordering:
+            # if 1 in hierarchy:
+            #     hierarchy.pop()
+            # else:
+            return idx + 1
+        else:
+            if ordering[-1][1] == idx:
+                if ordering[-1][0] == 1:
+                    return idx + 1
+                ordering.pop()
+            elif hierarchy and ordering[-1][0] in hierarchy:
+                if ordering[-1][0] == 1:
+                    return idx + 1
+                hierarchy.pop()
+            else:
+                return idx + 1
+
+
+with open("milkorder.out", "w") as f:
+    f.write(f"{solve(hierarchy, ordering, num_cows)}\n")
+
+# from rich import print as rprint
+
+# rprint(f"{hierarchy=}")
+# rprint(f"{ordering=}")
+# rprint(f"{solve(hierarchy, ordering, num_cows)=}")
 
 """
 3 4 5 1
+-> 4 is the answer
 """
