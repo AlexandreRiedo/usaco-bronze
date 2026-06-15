660c48fcc39ce65409af2a6d4db1f7a825603a75
Alexandre Riedo <alexandre.riedo@etu.unige.ch>
2026-06-15 13:04:30 +0200

    wip: usaco-bronze swap (12/13, very close!)

--- introduction-to-graphs/usaco-swap.py
+++ introduction-to-graphs/usaco-swap.py
@@ -1,4 +1,11 @@
-MODULO = 12
+# import random
+from collections import defaultdict
+
+# from math import gcd
+
+# from rich import print as rprint
+
+NAIVE_MODULO = 12
 
 with open("swap.in") as f:
     num_cows, num_repeats = map(int, f.readline().split())
@@ -18,29 +25,63 @@ def solve(a1, b1, a2, b2, num_cows, num_repeats):
     return order
 
 
+def find_modulo(a1, b1, a2, b2, num_cows):
+    orderings = set()
+    ordering_to_num_repeats = defaultdict(int)
+
+    for num_repeats in range(0, num_cows + 1):
+        ordering = tuple(solve(a1, b1, a2, b2, num_cows, num_repeats))
+
+        if ordering in orderings:
+            break
+        else:
+            orderings.add(ordering)
+            ordering_to_num_repeats[ordering] = num_repeats
+
+    # print(f"\n{orderings=}")
+    # print(f"cycle length: {len(orderings)}")
+
+    return len(orderings)
+
+
+MODULO = find_modulo(a1, b1, a2, b2, num_cows)
+
 with open("swap.out", "w") as f:
     for item in solve(a1, b1, a2, b2, num_cows, num_repeats % MODULO):
         f.write(f"{item}\n")
 
 # TESTING
-# num_cows = random.randint(1, 100)
-# a1, b1 = random.randint(1, num_cows - 1), random.randint(1, num_cows - 1)
-# a2, b2 = random.randint(a1 + 1, num_cows), random.randint(b1 + 1, num_cows)
+# unique_repeats = set()
 
-# rprint(f"{num_cows=} {a1=} {a2=} {b1=} {b2=}\n\n")
 
-# orderings = set()
-# ordering_to_num_repeats = defaultdict(int)
+# def test():
+#     num_cows = random.randint(2, 100)
+#     a1, b1 = random.randint(1, num_cows - 1), random.randint(1, num_cows - 1)
+#     a2, b2 = random.randint(a1 + 1, num_cows), random.randint(b1 + 1, num_cows)
 
-# for num_repeats in range(1, num_cows + 1):
-#     ordering = tuple(solve(a1, b1, a2, b2, num_cows, num_repeats))
+#     rprint(f"\n\n{num_cows=} {a1=} {a2=} {b1=} {b2=}")
 
-#     if ordering in orderings:
-#         continue
-#         rprint(f"{ordering=} with {num_repeats=} already seen in num_repeats={ordering_to_num_repeats[ordering]}")
-#     else:
-#         orderings.add(ordering)
-#         ordering_to_num_repeats[ordering] = num_repeats
+#     orderings = set()
+#     ordering_to_num_repeats = defaultdict(int)
 
-# rprint(f"\n{orderings=}")
-# rprint(f"cycle length: {max(ordering_to_num_repeats.values())}")
+#     for num_repeats in range(1, num_cows + 1):
+#         ordering = tuple(solve(a1, b1, a2, b2, num_cows, num_repeats))
+
+#         if ordering in orderings:
+#             continue
+#             rprint(
+#                 f"{ordering=} with {num_repeats=} already seen in num_repeats={ordering_to_num_repeats[ordering]}"
+#             )
+#         else:
+#             orderings.add(ordering)
+#             ordering_to_num_repeats[ordering] = num_repeats
+
+#     # rprint(f"\n{orderings=}")
+#     rprint(f"cycle length: {max(ordering_to_num_repeats.values())}")
+#     unique_repeats.add(max(ordering_to_num_repeats.values()))
+
+
+# for _ in range(100):
+#     test()
+# rprint(f"{sorted(unique_repeats)=}")
+# rprint(f"{gcd(*unique_repeats)=}")
