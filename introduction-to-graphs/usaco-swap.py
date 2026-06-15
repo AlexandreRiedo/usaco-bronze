dd77d30f8946d53344f78952e959ed4effb9c391
Alexandre Riedo <alexandre.riedo@etu.unige.ch>
2026-06-15 12:38:20 +0200

    wip: usaco-bronze swap (11/13, needs smarter modulo calculation)

--- introduction-to-graphs/usaco-swap.py
+++ introduction-to-graphs/usaco-swap.py
@@ -1,22 +1,46 @@
+MODULO = 12
+
 with open("swap.in") as f:
     num_cows, num_repeats = map(int, f.readline().split())
     a1, a2 = map(lambda x: int(x) - 1, f.readline().split())
     b1, b2 = map(lambda x: int(x) - 1, f.readline().split())
 
-order = list(range(1, num_cows + 1))
 
-for _ in range(num_repeats):
-    order[a1 : a2 + 1] = order[a2 : a1 - 1 : -1]
-    order[b1 : b2 + 1] = order[b2 : b1 - 1 : -1]
+def solve(a1, b1, a2, b2, num_cows, num_repeats):
+    order = list(range(1, num_cows + 1))
+
+    for _ in range(num_repeats):
+        # order[a1 : a2 + 1] = order[a2 : a1 - 1 : -1]
+        # order[b1 : b2 + 1] = order[b2 : b1 - 1 : -1]
+        order[a1 : a2 + 1] = reversed(order[a1 : a2 + 1])
+        order[b1 : b2 + 1] = reversed(order[b1 : b2 + 1])
+
+    return order
+
 
 with open("swap.out", "w") as f:
-    for item in order:
+    for item in solve(a1, b1, a2, b2, num_cows, num_repeats % MODULO):
         f.write(f"{item}\n")
 
+# TESTING
+# num_cows = random.randint(1, 100)
+# a1, b1 = random.randint(1, num_cows - 1), random.randint(1, num_cows - 1)
+# a2, b2 = random.randint(a1 + 1, num_cows), random.randint(b1 + 1, num_cows)
 
-"""
-1234567
+# rprint(f"{num_cows=} {a1=} {a2=} {b1=} {b2=}\n\n")
 
-1543267
-1576234
-"""
+# orderings = set()
+# ordering_to_num_repeats = defaultdict(int)
+
+# for num_repeats in range(1, num_cows + 1):
+#     ordering = tuple(solve(a1, b1, a2, b2, num_cows, num_repeats))
+
+#     if ordering in orderings:
+#         continue
+#         rprint(f"{ordering=} with {num_repeats=} already seen in num_repeats={ordering_to_num_repeats[ordering]}")
+#     else:
+#         orderings.add(ordering)
+#         ordering_to_num_repeats[ordering] = num_repeats
+
+# rprint(f"\n{orderings=}")
+# rprint(f"cycle length: {max(ordering_to_num_repeats.values())}")
