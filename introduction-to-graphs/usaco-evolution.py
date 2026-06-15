4a28e19c6e304fc665e6cb07f4f47db813722320
Alexandre Riedo <alexandre.riedo@etu.unige.ch>
2026-06-15 20:23:23 +0200

    lookup: usaco-bronze evolution test cases

--- introduction-to-graphs/usaco-evolution.py
+++ introduction-to-graphs/usaco-evolution.py
@@ -16,12 +16,12 @@ for at_idx, at in enumerate(subs):
         if at_idx == to_idx:
             continue
 
-        if at & to:
-            graph[at_idx].add(to_idx)
-            graph[to_idx].add(at_idx)
+        for char in at & to:
+            graph[at_idx].add((char, to_idx))
+            graph[to_idx].add((char, at_idx))
 
 with open("evolution.out", "w") as f:
-    if any(len(adj) > 1 for adj in graph):
+    if any(len({node[0] for node in adj}) > 1 for adj in graph):
         f.write("no\n")
     else:
         f.write("yes\n")
