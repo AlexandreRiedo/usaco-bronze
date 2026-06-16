0420f7f947106bbc3b758531663f40436802df91
Alexandre Riedo <alexandre.riedo@etu.unige.ch>
2026-06-17 00:55:31 +0200

    wip: usaco-bronze family (really ugly 3/15 solution)

---
+++ introduction-to-graphs/usaco-family-V1.py
@@ -0,0 +1,98 @@
+from collections import defaultdict
+
+with open("family.in") as f:
+    num_cows, a, b = f.readline().split()
+    num_cows = int(num_cows)
+
+    down = defaultdict(set)
+    up = defaultdict(set)
+
+    for _ in range(num_cows):
+        a, b = f.readline().split()
+        down[a].add(b)
+        up[b].add(a)
+
+
+def mutate_state(state, direction):
+    if direction == "up":
+        if state == "NOT RELATED":
+            state = "MOTHER"
+        elif state == "MOTHER":
+            state = "GRAND-MOTHER"
+        elif state == "GRAND-MOTHER":
+            state = "GREAT-GRAND-MOTHER"
+        elif "GREAT-GRAND-MOTHER" in state:
+            state = "GREAT-" + state
+        else:
+            state = "ILLEGAL GOING UP"
+    if direction == "down":
+        if state == "MOTHER":
+            state = "SISTER"
+        elif state == "GRAND-MOTHER":
+            state = "AUNT"
+        elif "GREAT-GRAND-MOTHER" in state:
+            state = state[0 : len(state) - 12] + "AUNT"
+        elif "AUNT" in state:
+            state = "COUSIN"
+        else:
+            state = "ILLEGAL GOING DOWN"
+    return state
+
+
+def rec_wrap():
+    def explore(curr, state, visited, target):
+        if curr in visited:
+            return
+        if curr == target:
+            nonlocal relation
+            relation = state
+            return
+
+        for parent in up[curr]:
+            visited.add(curr)
+            explore(parent, mutate_state(state, "up"), visited, target)
+            visited.remove(curr)
+        for child in down[curr]:
+            visited.add(curr)
+            explore(child, mutate_state(state, "down"), visited, target)
+            visited.remove(curr)
+
+    visited = set()
+    start = "AA"
+    target = "BB"
+    state = "NOT RELATED"
+    relation = "NOT RELATED"
+    explore(start, state, visited, target)
+
+    if "ILLEGAL" in relation:
+        visited = set()
+        start = "BB"
+        target = "AA"
+        state = "NOT RELATED"
+        relation = "NOT RELATED"
+        explore(start, state, visited, target)
+
+    return target, relation, start
+
+
+def format_answer(answer):
+    target, relation, start = answer
+    output = ""
+
+    if relation == "SISTER":
+        output = "SIBLINGS"
+    elif "MOTHER" in relation:
+        output = f"{target} is the {relation.lower()} of {start}"
+    elif "AUNT" in relation:
+        output = f"{target} is the {relation.lower()} of {start}"
+    elif relation == "COUSIN":
+        output = "COUSINS"
+    else:
+        output = "NOT RELATED"
+
+    return output
+
+
+with open("family.out", "w") as f:
+    answer = rec_wrap()
+    f.write(f"{format_answer(answer)}\n")
