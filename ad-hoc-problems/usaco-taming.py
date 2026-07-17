5f041796fcc4a53c2f19b152c06a7b4659eb69f8
Alexandre Riedo <alexandreriedopro@gmail.com>
2026-07-17 02:26:40 +0200

    solve: usaco-bronze Taming The Herd

---
+++ ad-hoc-problems/usaco-taming.py
@@ -0,0 +1,41 @@
+with open("taming.in") as f:
+    N = int(f.readline())
+    log = list(map(int, f.readline().split()))
+
+
+def solve(log):
+    m, M = 0, 0
+    counter = -1  # -1 -> unset
+
+    for idx in range(len(log) - 1, -1, -1):
+        entry = log[idx]
+
+        if counter != -1:
+            counter -= 1
+
+        if entry != -1 and counter != -1 and entry != counter:
+            return ["-1"]
+        elif entry != -1:
+            counter = entry
+
+        if idx == 0 and entry <= 0:
+            m += 1
+            M += 1
+        elif idx == 0 and entry > 0:
+            return ["-1"]
+        else:
+            if entry == -1:
+                if counter == -1:
+                    M += 1
+                elif counter == 0:
+                    m += 1
+                    M += 1
+            elif entry == counter == 0:
+                m += 1
+                M += 1
+
+    return [str(m), str(M)]
+
+
+with open("taming.out", "w") as f:
+    f.write(f"{' '.join(solve(log))}\n")
