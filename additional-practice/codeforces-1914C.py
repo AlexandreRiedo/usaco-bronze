35385fb999164708fb7184d54f8a71160aaa08f0
Alexandre Riedo <alexandreriedopro@gmail.com>
2026-07-19 19:45:59 +0200

    solve: codeforces 1914C

--- additional-practice/codeforces-1914C.py
+++ additional-practice/codeforces-1914C.py
@@ -4,33 +4,11 @@ for _ in range(t):
     points_a = list(map(int, input().split()))[:k]
     points_b = list(map(int, input().split()))[:k]
 
-    score = 0
-    for idx_b, b in enumerate(points_b):
-        curr_score = sum(points_a[: idx_b + 1])
-        remaining = k - (idx_b + 1)
-        max_b = max(points_b[: idx_b + 1])
-
-        curr_score += max_b * remaining
-        score = max(curr_score, score)
+    score, curr_a_score, max_b = 0, 0, 0
+    for idx, b in enumerate(points_b):
+        curr_a_score += points_a[idx]
+        remaining = k - (idx + 1)
+        max_b = max(max_b, b)
+        score = max(curr_a_score + max_b * remaining, score)
 
     print(score)
-
-"""
-1
-4 7
-4 3 1 2
-1 1 1 1
--> 4 + 3 + 1 + 2 + 1 + 1 + 1
-
-1
-5 5
-3 2 4 1 4
-2 3 1 4 7
--> 3 + 2 + 4 + 3 + 3
-
-1
-6 4
-1 4 5 4 5 10
-1 5 1 2 5 1
--> 1 + 4 + 5 + 5
-"""
