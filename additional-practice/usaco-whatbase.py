448d412351e40f5128c2caad5e59a8ad690fda6a
Alexandre Riedo <alexandreriedopro@gmail.com>
2026-08-29 14:11:07 +0200

    solve: usaco-bronze It's All About the Base

--- additional-practice/usaco-whatbase.py
+++ additional-practice/usaco-whatbase.py
@@ -8,16 +8,16 @@ def convert(base10_num: int, base: int) -> int:
 with open("whatbase.in", "r") as fin, open("whatbase.out", "w") as fout:
     for _ in range(int(fin.readline())):
         in_num_X, in_num_Y = map(int, fin.readline().split())
-
         num_X, num_Y = min(in_num_X, in_num_Y), max(in_num_X, in_num_Y)
 
         base_X, base_Y = 10, 10
-        while convert(num_X, base_X) != convert(num_Y, base_Y):
-            new_X, new_Y = convert(num_X, base_X), convert(num_Y, base_Y)
+        new_X, new_Y = convert(num_X, base_X), convert(num_Y, base_Y)
+        while new_X != new_Y:
             if new_X < new_Y:
                 base_X += 1
             else:
                 base_Y += 1
+            new_X, new_Y = convert(num_X, base_X), convert(num_Y, base_Y)
 
         if num_X == in_num_X:
             fout.write(f"{base_X} {base_Y}\n")
