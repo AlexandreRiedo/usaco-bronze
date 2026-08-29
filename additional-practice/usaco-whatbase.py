229cef67e2dba013de68f806e53d275fac05408a
Alexandre Riedo <alexandreriedopro@gmail.com>
2026-08-29 14:06:39 +0200

    wip: usasco-bronze It's All About The Base (7/11)

---
+++ additional-practice/usaco-whatbase.py
@@ -0,0 +1,25 @@
+def convert(base10_num: int, base: int) -> int:
+    ans = 0
+    for idx, digit in enumerate(reversed(list(map(int, str(base10_num))))):
+        ans += digit * (base**idx)
+    return ans
+
+
+with open("whatbase.in", "r") as fin, open("whatbase.out", "w") as fout:
+    for _ in range(int(fin.readline())):
+        in_num_X, in_num_Y = map(int, fin.readline().split())
+
+        num_X, num_Y = min(in_num_X, in_num_Y), max(in_num_X, in_num_Y)
+
+        base_X, base_Y = 10, 10
+        while convert(num_X, base_X) != convert(num_Y, base_Y):
+            new_X, new_Y = convert(num_X, base_X), convert(num_Y, base_Y)
+            if new_X < new_Y:
+                base_X += 1
+            else:
+                base_Y += 1
+
+        if num_X == in_num_X:
+            fout.write(f"{base_X} {base_Y}\n")
+        else:
+            fout.write(f"{base_Y} {base_X}\n")
