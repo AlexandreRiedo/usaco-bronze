3468db2070412e32392b16f98ffe7991a74c4030
Alexandre Riedo <alexandreriedopro@gmail.com>
2026-09-03 23:02:07 +0200

    wip: usaco-bronze Purchasing Milk (5/16)

---
+++ introduction-to-greedy-algorithms/usaco-purchasingmilk.py
@@ -0,0 +1,18 @@
+import math
+
+n, q = map(int, input().split())
+deals = [math.inf] * (10**5)
+for idx, deal in enumerate(map(int, input().split())):
+    deals[idx] = deal
+for i in range(1, len(deals)):
+    deals[i] = min(deals[i], deals[i - 1] * 2)
+
+
+for _ in range(q):
+    x = list(map(int, str(bin(int(input())))[:1:-1]))
+
+    ans = 0
+    for exponent, code in enumerate(x):
+        ans += deals[exponent] * code
+
+    print(ans)
