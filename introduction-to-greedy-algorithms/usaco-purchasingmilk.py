f36a39aa9e608f3000f2ddd7140f7e37a1b3490e
Alexandre Riedo <alexandreriedopro@gmail.com>
2026-09-03 23:13:38 +0200

    wip: usaco-bronze purchasing milk (binary search odea ?)

--- introduction-to-greedy-algorithms/usaco-purchasingmilk.py
+++ introduction-to-greedy-algorithms/usaco-purchasingmilk.py
@@ -1,18 +1,20 @@
 import math
+from bisect import bisect_right
 
 n, q = map(int, input().split())
-deals = [math.inf] * (10**5)
+deals = [math.inf] * (10**5 + 100)
 for idx, deal in enumerate(map(int, input().split())):
     deals[idx] = deal
 for i in range(1, len(deals)):
     deals[i] = min(deals[i], deals[i - 1] * 2)
 
-
 for _ in range(q):
-    x = list(map(int, str(bin(int(input())))[:1:-1]))
+    x = list(map(int, bin(int(input()))[:1:-1]))
 
     ans = 0
     for exponent, code in enumerate(x):
         ans += deals[exponent] * code
+
+    ans = min(ans, deals[bisect_right(deals, ans)])
 
     print(ans)
