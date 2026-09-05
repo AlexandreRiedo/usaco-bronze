1680e083643fd3fdcb8c1b6e737b324a42de7835
Alexandre Riedo <alexandreriedopro@gmail.com>
2026-09-05 15:30:05 +0200

    lookup: usaco-bronze Purchasing Milk (Claude's Hint)

--- introduction-to-greedy-algorithms/usaco-purchasingmilk.py
+++ introduction-to-greedy-algorithms/usaco-purchasingmilk.py
@@ -1,10 +1,11 @@
 import math
 
-from rich import print as rprint
+MAX_QUERY = 10**9
+NUM_DEALS = math.ceil(math.log2(MAX_QUERY))
 
 n, q = map(int, input().split())
 
-deals = [math.inf] * (10**5 + 100)
+deals = [math.inf] * (NUM_DEALS)
 for idx, deal in enumerate(map(int, input().split())):
     deals[idx] = deal
 for i in range(1, len(deals)):
@@ -13,11 +14,17 @@ for i in range(1, len(deals)):
 for _ in range(q):
     x = list(map(int, bin(int(input()))[:1:-1]))
 
-    ans = 0
-    exponent = 0
+    ans, exponent = 0, 0
     for exponent, code in enumerate(x):
         ans += deals[exponent] * code
 
     ans = min(ans, deals[exponent + 1])
 
-    rprint(ans)
+    print(ans)
+
+"""
+CLAUDE Hint:
+5 1
+5 6 7 8 100
+11
+"""
