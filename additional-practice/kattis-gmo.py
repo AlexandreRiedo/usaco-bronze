505c0fabef41ce0b4ca23aa6df90492d4d20adda
Alexandre Riedo <alexandreriedopro@gmail.com>
2026-09-06 21:21:13 +0200

    wip: kattis GMO (trying some heuristics)

--- additional-practice/kattis-gmo.py
+++ additional-practice/kattis-gmo.py
@@ -1,5 +1,5 @@
-apple = input()
-swine = input()
+a = input()
+s = input()
 costs = {["A", "C", "G", "T"][i]: int(v) for i, v in enumerate(input().split())}
 
 
@@ -7,9 +7,8 @@ def seq_cost(s):
     return sum(costs[c] for c in s)
 
 
-def insert_cost(a: str, s: str, a_init: int):
-    cost = seq_cost(s)
-    s_idx = s.find(a[a_init])
+def insert_cost(a: str, s: str, a_init: int, cost: int, init_find: dict[str, int]):
+    s_idx = init_find[a[a_init]]
 
     for a_char in a[a_init:]:
         if a_char not in s[s_idx:] or s_idx >= len(s) or s_idx == -1:
@@ -21,24 +20,6 @@ def insert_cost(a: str, s: str, a_init: int):
     return cost
 
 
-print(min(insert_cost(apple, swine, idx) for idx in range(len(apple))))
-
-"""
-GTA
-CAT
-A C G T
-5 7 1 3
--> 10 (GTCAT)
-
-TATA
-CACA
-A C G T
-3 0 3 0
--> 3 (TCACATA or TATCACA)
-
-TCGCGAG
-TGCAG
-A  C  G  T
-10 10 15 15
--> 25 (TGCAG CGAG OR TCTGCAG)
-"""
+f = {c: s.find(c) for c in ["A", "C", "G", "T"]}
+cost = seq_cost(s)
+print(min(insert_cost(a, s, idx, cost, f) for idx in range(len(a))))
