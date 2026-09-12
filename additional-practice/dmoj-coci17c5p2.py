997bb87810454c04563da5a3d2903124588f933b
Alexandre Riedo <alexandreriedopro@gmail.com>
2026-09-12 12:59:29 +0200

    wip: DMOJ Spirale (6/7, python too slow!)

--- additional-practice/dmoj-coci17c5p2.py
+++ additional-practice/dmoj-coci17c5p2.py
@@ -1,41 +1,60 @@
-from itertools import accumulate
 from math import inf
 
+
+def add(x, y, val, table, x_len, y_len):
+    if 0 <= x < x_len and 0 <= y < y_len:
+        table[y][x] = min(val, table[y][x])
+
+
+def assign_spiral(x: int, y: int, direction: int, table: list[list]):
+    val = 1
+    zone = 2
+    x_len, y_len = len(table[0]), len(table)
+    init_x, init_y = x, y
+    cap = max(max(init_x, x_len - init_x), max(init_y, y_len - init_y))
+
+    add(x, y, val, table, x_len, y_len)
+    y -= 1
+
+    while (zone / 2) < cap:
+        if direction == 0:
+            for x in range(x, x + zone):
+                val += 1
+                add(x, y, val, table, x_len, y_len)
+            for y in range(y + 1, y + 1 + zone):
+                val += 1
+                add(x, y, val, table, x_len, y_len)
+            for x in range(x - 1, x - 1 - zone, -1):
+                val += 1
+                add(x, y, val, table, x_len, y_len)
+            for y in range(y - 1, y - 1 - zone, -1):
+                val += 1
+                add(x, y, val, table, x_len, y_len)
+        else:
+            for x in range(x, x - zone, -1):
+                val += 1
+                add(x, y, val, table, x_len, y_len)
+            for y in range(y + 1, y + 1 + zone):
+                val += 1
+                add(x, y, val, table, x_len, y_len)
+            for x in range(x + 1, x + 1 + zone):
+                val += 1
+                add(x, y, val, table, x_len, y_len)
+            for y in range(y - 1, y - 1 - zone, -1):
+                val += 1
+                add(x, y, val, table, x_len, y_len)
+
+        y -= 1
+        zone += 2
+
+
 num_rows, num_cols, num_spirals = map(int, input().split())
-spirals = []
+table = [[inf for _ in range(num_cols)] for _ in range(num_rows)]
 for _ in range(num_spirals):
-    x, y, t = map(int, input().split())
+    y, x, t = map(int, input().split())
     x -= 1
     y -= 1
-    spirals.append((x, y, t))
+    assign_spiral(x, y, t, table)
 
-cells_upto = list(accumulate([1] + [4 * x - 4 for x in range(1, 100, 2)][1:]))
-table = [[inf for _ in range(num_cols)] for _ in range(num_rows)]
-
-
-def assign_spiral(x: int, y: int, direction: int, table: list[list[int]]):
-    seen_tl, seen_br = False, False
-
-    val = 1
-    table[y][x] = min(val, table[y][x])
-
-    offset = 1
-    y -= 1
-
-    while not seen_tl or not seen_br:
-        if direction == 0:
-            pass
-        elif direction == 1:
-            print()
-
-        if y == 0 and x == 0:
-            seen_tl = True
-        if y == len(table) - 1 and x == len(table[0]) - 1:
-            seen_br = True
-
-
-"""
-sizes = [4*x - 4 for x in range(1,50,2)]
-t = list(accumulate([1]+sizes[1:]))
-print(t)
-"""
+for row in table:
+    print(*row)
