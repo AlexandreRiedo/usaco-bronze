2b68a55a99d013381fc525472330cfc73bf63e06
Alexandre Riedo <alexandreriedopro@gmail.com>
2026-09-12 13:16:29 +0200

    solve: DMOJ Spirale (6/7, but even the editorial can't do it in time!)

--- additional-practice/dmoj-coci17c5p2.py
+++ additional-practice/dmoj-coci17c5p2.py
@@ -1,53 +1,57 @@
 from math import inf
 
 
-def add(x, y, val, table, x_len, y_len):
+def add(x, y, val, table):
+    global visited
+    x_len, y_len = len(table[0]), len(table)
+
     if 0 <= x < x_len and 0 <= y < y_len:
         table[y][x] = min(val, table[y][x])
+        visited += 1
 
 
 def assign_spiral(x: int, y: int, direction: int, table: list[list]):
+    global visited
     val = 1
     zone = 2
-    x_len, y_len = len(table[0]), len(table)
-    init_x, init_y = x, y
-    cap = max(max(init_x, x_len - init_x), max(init_y, y_len - init_y))
 
-    add(x, y, val, table, x_len, y_len)
+    add(x, y, val, table)
     y -= 1
+    visited = 1
 
-    while (zone / 2) < cap:
+    while visited < len(table[0]) * len(table):
         if direction == 0:
             for x in range(x, x + zone):
                 val += 1
-                add(x, y, val, table, x_len, y_len)
+                add(x, y, val, table)
             for y in range(y + 1, y + 1 + zone):
                 val += 1
-                add(x, y, val, table, x_len, y_len)
+                add(x, y, val, table)
             for x in range(x - 1, x - 1 - zone, -1):
                 val += 1
-                add(x, y, val, table, x_len, y_len)
+                add(x, y, val, table)
             for y in range(y - 1, y - 1 - zone, -1):
                 val += 1
-                add(x, y, val, table, x_len, y_len)
+                add(x, y, val, table)
         else:
             for x in range(x, x - zone, -1):
                 val += 1
-                add(x, y, val, table, x_len, y_len)
+                add(x, y, val, table)
             for y in range(y + 1, y + 1 + zone):
                 val += 1
-                add(x, y, val, table, x_len, y_len)
+                add(x, y, val, table)
             for x in range(x + 1, x + 1 + zone):
                 val += 1
-                add(x, y, val, table, x_len, y_len)
+                add(x, y, val, table)
             for y in range(y - 1, y - 1 - zone, -1):
                 val += 1
-                add(x, y, val, table, x_len, y_len)
+                add(x, y, val, table)
 
         y -= 1
         zone += 2
 
 
+visited = 0
 num_rows, num_cols, num_spirals = map(int, input().split())
 table = [[inf for _ in range(num_cols)] for _ in range(num_rows)]
 for _ in range(num_spirals):
