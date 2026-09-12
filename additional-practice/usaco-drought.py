099e0d5af36deda22958fec616e1aaba6bb9e85c
Alexandre Riedo <alexandreriedopro@gmail.com>
2026-09-12 08:34:30 +0200

    lookup: usaco-bronze Drought (editorial implementation)

---
+++ additional-practice/usaco-drought.py
@@ -0,0 +1,36 @@
+for _ in range(int(input())):
+    N = int(input())
+    h = list(map(int, input().split()))
+    h_og = h.copy()
+
+    for i in range(len(h)):
+        if i == len(h) - 1:
+            continue
+        elif i == len(h) - 2:
+            if h[i] < h[i + 1] or (len(h) == 2 and h[i] != h[i + 1]):
+                print(-1)
+                break
+            else:
+                h[: i + 1] = [h[i + 1]] * len(h[: i + 1])
+        else:
+            if h[i + 1] > h[i]:
+                d = h[i + 1] - h[i]
+                h[i + 1] -= d
+                h[i + 2] -= d
+                if h[i + 2] < 0:
+                    print(-1)
+                    break
+            elif h[i + 1] < h[i]:
+                if i % 2 == 0:
+                    print(-1)
+                    break
+                else:
+                    h[: i + 1] = [h[i + 1]] * len(h[: i + 1])
+    else:
+        print(sum(a - b for a, b in zip(h_og, h)))
+
+"""
+1
+3
+8 10 5
+"""
