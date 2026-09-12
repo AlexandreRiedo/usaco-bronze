55e8d5a95798858b48815fff0587dbc46776a6e2
Alexandre Riedo <alexandreriedopro@gmail.com>
2026-09-12 15:47:38 +0200

    lookup: usaco-bronze Just Stalling

--- additional-practice/usaco-juststalling.py
+++ additional-practice/usaco-juststalling.py
@@ -1,41 +1,21 @@
-from bisect import bisect_right as br
+def prod(a):
+    ans = a[0]
+    for num in a[1:]:
+        ans *= num
+    return ans
+
 
 N = int(input())
-cows = sorted(map(int, input().split()))
+cows = sorted(map(int, input().split()), reverse=True)
 stalls = sorted(map(int, input().split()))
 
-pools = []
-for h in stalls:
-    pools.append(cows[: br(cows, h)])
-
-
-def wrap(pools):
-    global count
-    count = 0
-
-    def explore(used, pools):
-        global count
-
-        if len(used) == len(pools):
-            count += 1
-            return
-        else:
-            for cow in pools[len(used)]:
-                if used.count(cow) == pools[len(used)].count(cow):
-                    continue
-                else:
-                    used.append(cow)
-                    explore(used, pools)
-                    used.pop()
-
-    explore([], pools)
-
-    return count
+ans = []
+for idx, c in enumerate(cows):
+    ans.append(sum(c <= h for h in stalls) - idx)
+print(prod(ans))
 
 
 # print(sum(all(c <= h for c, h in zip(o, stalls)) for o in permutations(cows, N)))
-print(wrap(pools))
-
 # rprint(*[o for o in permutations(cows, N) if all(c <= h for c, h in zip(o, stalls))])
 
 """
