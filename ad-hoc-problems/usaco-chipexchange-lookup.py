cdba546430ebb325ae59874666d77cd0d46b9979
Alexandre Riedo <alexandreriedopro@gmail.com>
2026-09-13 13:52:51 +0200

    lookup: usaco-bronze Chip Exchange (bonkers hard problem)

--- ad-hoc-problems/usaco-chipexchange-lookup.py
+++ ad-hoc-problems/usaco-chipexchange-lookup.py
@@ -1,8 +1,3 @@
-from math import inf
-
-SUB = 10
-
-
 def verify(A, B, cA, cB, fA, nA, nB):
     return cA * ((B + nB) // cB) + (A + nA) >= fA
 
@@ -12,14 +7,17 @@ def solve(A, B, cA, cB, fA):
     if init >= fA:
         return 0
 
-    ans = inf
-    for x in range(SUB + 1):
-        if all(
-            verify(A, B, cA, cB, fA, nA, nB)
-            for nA, nB in zip(range(x + 1), range(x, -1, -1))
-        ):
-            ans = min(ans, x)
-    return ans
+    nB0 = cB - 1 - (B % cB)
+    nA0 = fA - 1 - init
+
+    if cA >= cB:
+        i = 0
+    else:
+        i = nA0 // cA
+
+    nA, nB = nA0 - cA * i, nB0 + i * cB
+
+    return 1 + nA + nB
 
 
 for _ in range(int(input())):
