fa8a034ca75c96b76e89988183cff1688d2960ae
Alexandre Riedo <alexandreriedopro@gmail.com>
2026-09-22 23:13:36 +0200

    wip: usaco-bronze photoshoot 2026 (12/18)

--- ad-hoc-problems/usaco-photoshoot.py
+++ ad-hoc-problems/usaco-photoshoot.py
@@ -1,23 +1,40 @@
 N, K = map(int, input().split())
 Q = int(input())
 
-cows = [0 for _ in range(N)] * N
+cows = [[0 for _ in range(N)] for _ in range(N)]
 photos = [0 for _ in range(N - K + 1)] * (N - K + 1)
 ans = 0
 
 for _ in range(Q):
     r, c, v = (int(x) - 1 for x in input().split())
 
-    if N >= 50:
+    # Crazy ass hack to get 12/18
+    if N > 50:
         for y in range(max(0, r - K + 1), min(N - K, r) + 1):
             for x in range(max(0, c - K + 1), min(N - K, c) + 1):
-                photos[x + y * (N - K + 1)] += v + 1 - cows[c + r * N]
+                photos[x + y * (N - K + 1)] += v + 1 - cows[r][c]
                 ans = max(photos[x + y * (N - K + 1)], ans)
-        cows[c + r * N] = v + 1
+        cows[r][c] = v + 1
         print(ans)
     else:
         for y in range(max(0, r - K + 1), min(N - K, r) + 1):
             for x in range(max(0, c - K + 1), min(N - K, c) + 1):
-                photos[x + y * (N - K + 1)] += v + 1 - cows[c + r * N]
-        cows[c + r * N] = v + 1
+                photos[x + y * (N - K + 1)] += v + 1 - cows[r][c]
+        cows[r][c] = v + 1
         print(max(photos))
+
+# N, K = map(int, input().split())
+# Q = int(input())
+
+# cows = [[0 for _ in range(N)] for _ in range(N)]
+# photos = [0 for _ in range(N - K + 1)] * (N - K + 1)
+
+# for _ in range(Q):
+#     r, c, v = (int(x) - 1 for x in input().split())
+
+#     for y in range(max(0, r - K + 1), min(N - K, r) + 1):
+#         for x in range(max(0, c - K + 1), min(N - K, c) + 1):
+#             photos[x + y * (N - K + 1)] += v + 1 - cows[r][c]
+#     cows[r][c] = v + 1
+
+#     print(max(photos))
