43ebc0730158c9ad30488c778d71439e565f5f78
Alexandre Riedo <alexandreriedopro@gmail.com>
2026-09-22 22:20:24 +0200

    wip: usaco-bronze photoshoot 2026 (6/18)

--- ad-hoc-problems/usaco-photoshoot.py
+++ ad-hoc-problems/usaco-photoshoot.py
@@ -1,18 +1,18 @@
 N, K = map(int, input().split())
 cows = [[0 for _ in range(N)] for _ in range(N)]
 Q = int(input())
+ans = 0
 
 for _ in range(Q):
     r, c, v = (int(x) - 1 for x in input().split())
     cows[r][c] = v + 1
 
-    ans = 0
-    for y in range(max(0, N - K + 1)):
-        for x in range(max(0, N - K + 1)):
+    for y in range(max(0, r - K + 1), min(N - K, r) + 1):
+        for x in range(max(0, c - K + 1), min(N - K, c) + 1):
             curr = 0
-            for r in range(x, x + K):
-                for c in range(y, y + K):
-                    curr += cows[r][c]
+            for curr_c in range(x, x + K):
+                for curr_r in range(y, y + K):
+                    curr += cows[curr_r][curr_c]
             ans = max(curr, ans)
 
     print(ans)
