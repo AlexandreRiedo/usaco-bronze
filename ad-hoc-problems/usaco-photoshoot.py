52c0cf266033ef7927117f8e120a605a427cd605
Alexandre Riedo <alexandreriedopro@gmail.com>
2026-09-22 22:56:03 +0200

    wip: usaco-bronze photoshoot 2026 (11/18)

--- ad-hoc-problems/usaco-photoshoot.py
+++ ad-hoc-problems/usaco-photoshoot.py
@@ -3,6 +3,7 @@ Q = int(input())
 
 cows = [[0 for _ in range(N)] for _ in range(N)]
 photos = [0 for _ in range(N - K + 1)] * (N - K + 1)
+ans = 0
 
 for _ in range(Q):
     r, c, v = (int(x) - 1 for x in input().split())
@@ -10,9 +11,10 @@ for _ in range(Q):
     for y in range(max(0, r - K + 1), min(N - K, r) + 1):
         for x in range(max(0, c - K + 1), min(N - K, c) + 1):
             photos[x + y * (N - K + 1)] += v + 1 - cows[r][c]
+            ans = max(photos[x + y * (N - K + 1)], ans)
     cows[r][c] = v + 1
 
-    print(max(photos))
+    print(ans)
 
 
 """
